b08f64c9003f1710ae942321a7bf275909b98b5a
austenFs <aust.furu@gmail.com>
2022-02-22 12:43:44 -0800

    Added coms

--- app/src/main/java/com/example/cs301_battleship/board.java
+++ app/src/main/java/com/example/cs301_battleship/board.java
@@ -3,6 +3,12 @@ package com.example.cs301_battleship;
 public class board extends coordinates {
     private coordinates currentBoard[][] = new coordinates[10][10];
 
+    /**
+     * Default constructor for the board
+     * initerates through the 2d array assigning each one
+     * coordinate object with two false values one for hit(if the coordinate has been fired at)
+     * and one for hasShip(Identifying if the coordinate houses a ship)
+     */
     public board(){
         int i,j;
         for (i = 1; i <= 10; i++){
@@ -12,6 +18,13 @@ public class board extends coordinates {
         }
     }
 
+
+    /**
+     * Copy constructor for the Board object
+     * iterates through the list assigning each coordinate with
+     * a copy of the originals coordinate values
+     * @param orig
+     */
     public board(board orig){ //copy constructor
         int i,j;
         for (i = 1; i <= 10; i++){
@@ -21,12 +34,26 @@ public class board extends coordinates {
         }
     }
 
-
- public board(int row, int col, boolean hit){
-        boolean wasHit = this.currentBoard[row][col].setHit(hit);
-
+    /**
+     * Sets the coordinate of row, col to be hit or not hit
+     * @param row
+     * @param col
+     * @param hit
+     */
+ public boolean setCoorhit(int row, int col, boolean hit){
+     if(getCoordHit(row, col) == false) {
+         this.currentBoard[row][col].setHit(hit);
+         return true;
+     }
+     return false;
  }
 
+    /**
+     * Checks if the coordinate values passed in has been hit
+     * @param row
+     * @param col
+     * @return bool, if the coordinate has been hit
+     */
  public boolean getCoordHit(int row, int col){
         return this.currentBoard[row][col].getHit();
  }
--- app/src/main/java/com/example/cs301_battleship/coordinates.java
+++ app/src/main/java/com/example/cs301_battleship/coordinates.java
@@ -4,25 +4,48 @@ public class coordinates {
     private boolean hit;
     private boolean hasShip;
 
+    /**
+     * creates a coordinate object setting its
+     * @param hit
+     * @param hasShip
+     */
     public coordinates(boolean hit, boolean hasShip){
         this.hit = hit;
         this.hasShip = hasShip;
     }
+
+    /**
+     * default constructor for coordinate sets hit and hasShip to false
+     */
     public coordinates(){ //default constructor
         this.hit = false;
         this.hasShip = false;
     }
 
-    public coordinates(coordinates orig){ //copy constructor
+    /**
+     * Copy contructor, creates a copy of the passed in coordinate object
+     * @param orig
+     */
+    public coordinates(coordinates orig){
         this.hit = orig.hit;
         this.hasShip = orig.hasShip;
     }
 
+    /**
+     * Changes the boolean of hit to the passed in value, then returns
+     * if the coordinate hit has a ship on it
+     * @param hit
+     * @return
+     */
     public boolean setHit(boolean hit){
         this.hit = hit;
         return this.hasShip;
     }
 
+    /**
+     * returns a boolean, if the coordinate has already been hit.
+     * @return bool of hit
+     */
     public boolean getHit() {
         return this.hit;
     }
