52c4b563f08e2df9e92d5051b9c846fabb842721
7angsan <leeste25@up.edu>
2022-02-22 11:54:39 -0800

    BattleShipGameState Class: - Added instance variables - Added a normal constructor - Added a copy constructor

---
+++ app/src/main/java/com/example/cs301_battleship/BattleShipGameState.java
@@ -0,0 +1,33 @@
+package com.example.cs301_battleship;
+
+public class BattleShipGameState {
+    String playerID;
+    Board playersBoard;
+    boolean playersTurn;
+    int timer;
+    // 0 = setup | 1 = game phase | 2 = end phase
+    int phase;
+    int remainingShips;
+
+
+
+    public BattleShipGameState(String playerID, Board playersBoard, boolean playersTurn, int timer, int phase, int remainingShips) {
+        this.playerID = playerID;
+        this.playersBoard = playersBoard;
+        this.playersTurn = playersTurn;
+        this.timer = timer;
+        this.phase = phase;
+        this.remainingShips = remainingShips;
+    }
+
+    public BattleShipGameState(BattleShipGameState copy) {
+        this.playerID = copy.playerID;
+        this.playersBoard = copy.playersBoard;
+        this.playersTurn = copy.playersTurn;
+        this.timer = copy.timer;;
+        this.phase = copy.phase;
+        this.remainingShips = copy.remainingShips;
+    }
+
+
+}
