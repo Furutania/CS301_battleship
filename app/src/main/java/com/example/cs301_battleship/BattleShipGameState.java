9d18a18d61df008282971be79cd24c1dc7325e2b
7angsan <leeste25@up.edu>
2022-02-22 12:34:13 -0800

    Comments for Game State.

--- app/src/main/java/com/example/cs301_battleship/BattleShipGameState.java
+++ app/src/main/java/com/example/cs301_battleship/BattleShipGameState.java
@@ -1,16 +1,16 @@
 package com.example.cs301_battleship;
 
 public class BattleShipGameState {
-    String playerID;
-    board playersBoard;
-    boolean playersTurn;
-    int timer;
+    private String playerID;
+    private board playersBoard;
+    private boolean playersTurn;
+    private int timer;
     // 0 = setup | 1 = game phase | 2 = end phase
-    int phase;
-    int remainingShips;
+    private int phase;
+    private int remainingShips;
 
 
-
+    // basic constructor
     public BattleShipGameState(String playerID, board playersBoard, boolean playersTurn, int timer, int phase, int remainingShips) {
         this.playerID = playerID;
         this.playersBoard = playersBoard;
@@ -20,6 +20,7 @@ public class BattleShipGameState {
         this.remainingShips = remainingShips;
     }
 
+    // deep copy constructor
     public BattleShipGameState(BattleShipGameState copy) {
         this.playerID = copy.playerID;
         this.playersBoard = new board(copy.playersBoard);
@@ -29,6 +30,7 @@ public class BattleShipGameState {
         this.remainingShips = copy.remainingShips;
     }
 
+    // if true, then player can fire. if false, then player can't fire
     public boolean canFire(int row, int col) {
         boolean isHit = playersBoard.getCoordHit(row, col);
         if (isHit == true) {
@@ -38,6 +40,7 @@ public class BattleShipGameState {
     }
 
     @Override
+    // returns the number of ships remaining for the player if it is their turn or returns that it is not their turn
     public String toString() {
         if (playersTurn == true) {
             return "It is " + playerID + "'s turn." + playerID + "has " + remainingShips + " remaining.";
