371258baf85661965b99a2ec658922d7ac94fe22
7angsan <leeste25@up.edu>
2022-03-17 10:22:18 -0700

    Added GameComputerPlayer, GameHumanPlayer classes.

---
+++ app/src/main/java/com/example/cs301_battleship/GameComputerPlayer.java
@@ -0,0 +1,33 @@
+package com.example.cs301_battleship;
+
+public class GameComputerPlayer implements GamePlayer {
+    @Override
+    public void gameSetAsGui(MainActivity activity) {
+
+    }
+
+    @Override
+    public void setAsGui(MainActivity activity) {
+
+    }
+
+    @Override
+    public void sendInfo(BattleShipGameState info) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public boolean requiresGui() {
+        return false;
+    }
+
+    @Override
+    public boolean supportsGui() {
+        return false;
+    }
+}
