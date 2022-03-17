de1935e135996235c5fdd1b74b7e7b293dfc43a9
austenFs <aust.furu@gmail.com>
2022-03-17 09:57:15 -0700

    Updated Classes from proj e

--- app/src/main/java/com/example/cs301_battleship/GameBoard.java
+++ app/src/main/java/com/example/cs301_battleship/GameBoard.java
@@ -1,4 +1,6 @@
 package com.example.cs301_battleship;
+import android.util.Log;
+
 /**
  * board - Contains a 2d array of coordinates, each coordinate has two booleans
  * hit and hasShip from.
@@ -10,61 +12,65 @@ package com.example.cs301_battleship;
  * @version Spring 2022 - 2/22/22
  */
 public class GameBoard extends Coordinates {
-    private Coordinates currentBoard[][] = new Coordinates[10][10];
+    private Coordinates currentBoard[][]; //a Coordinate array representing the board
 
     /**
-     * Default constructor for the board
+     * GameBoard - Default constructor for the board
      * initerates through the 2d array assigning each one
      * coordinate object with two false values one for hit(if the coordinate has been fired at)
      * and one for hasShip(Identifying if the coordinate houses a ship)
      */
     public GameBoard(){
         int i,j;
-        for (i = 1; i <= 10; i++){
-            for(j = 1; i <= 10; j++){
-                this.currentBoard[i][j] = new Coordinates(false, false, i, j);
+        this.currentBoard = new Coordinates[10][10];
+        for (i = 0; i < 10; i++)  {
+            for(j = 0; j < 10; j++){
+                this.currentBoard[i][j] = new Coordinates(false, false, i + 1, j + 1);
             }
         }
     }
 
 
     /**
-     * Copy constructor for the Board object
+     * GameBoard - Copy constructor for the Board object
      * iterates through the list assigning each coordinate with
      * a copy of the originals coordinate values
-     * @param orig
+     * @param orig - a copy of the original GameBoard
      */
     public GameBoard(GameBoard orig){ //copy constructor
         int i,j;
-        for (i = 1; i <= 10; i++){
-            for(j = 1; i <= 10; j++){
+        this.currentBoard = new Coordinates[10][10];
+        for (i = 0; i < 10; i++){
+            for(j = 0; j < 10; j++){
+                Log.i("COPY", "GameBoard: " + i + " " + j);
                 this.currentBoard[i][j] = new Coordinates(orig.currentBoard[i][j]);
             }
         }
+        Log.i("DONE", "DONE");
     }
 
     /**
-     * Sets the coordinate of row, col to be hit or not hit
-     * @param row
-     * @param col
-     * @param hit
+     * setCoordHit - Sets the coordinate of row, col to be hit or not hit
+     * @param row - a given row coord
+     * @param col - a given column coord
+     * @param hit - whether the ship has been hit or not
      */
- public boolean setCoordHit(int row, int col, boolean hit){
-     if(getCoordHit(row, col) == false) {
-         this.currentBoard[row][col].setHit(hit);
-         return true;
-     }
-     return false;
- }
+    public boolean setCoordHit(int row, int col, boolean hit){
+        if(getCoordHit(row, col) == false) {
+            this.currentBoard[row][col].setHit(hit);
+            return true;
+        }
+        return false;
+    }
 
     /**
      * Checks if the coordinate values passed in has been hit
-     * @param row
-     * @param col
+     * @param row - a given row coord
+     * @param col - a given column coord
      * @return bool, if the coordinate has been hit
      */
- public boolean getCoordHit(int row, int col){
+    public boolean getCoordHit(int row, int col){
         return this.currentBoard[row][col].getHit();
- }
+    }
 
 }
