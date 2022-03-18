8338d0f2adf6b9738c91c2b0613950ee839a6d2e
7angsan <leeste25@up.edu>
2022-03-18 16:31:17 -0700

    SmartAI class added.

---
+++ app/src/main/java/com/example/cs301_battleship/SmartAI.java
@@ -0,0 +1,18 @@
+package com.example.cs301_battleship;
+
+import android.util.Log;
+
+import java.util.Random;
+
+public class SmartAI extends GameComputerPlayer {
+
+    public SmartAI() {
+        super();
+    }
+
+    @Override
+    protected void receiveInfo(BattleShipGameState info) {
+
+
+    }
+}
