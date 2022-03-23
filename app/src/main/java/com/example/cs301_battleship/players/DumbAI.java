1646369c664ff4025b959168e45d5b8686e7d5e1
7angsan <leeste25@up.edu>
2022-03-23 00:20:00 -0700

    Added code to AI.

--- app/src/main/java/com/example/cs301_battleship/players/DumbAI.java
+++ app/src/main/java/com/example/cs301_battleship/players/DumbAI.java
@@ -3,6 +3,10 @@ package com.example.cs301_battleship.players;
 import android.util.Log;
 
 import com.example.cs301_battleship.BattleShipGameState;
+import com.example.cs301_battleship.Coordinates;
+import com.example.cs301_battleship.GameAction.Fire;
+import com.example.cs301_battleship.GameAction.PlaceShip;
+import com.example.cs301_battleship.Gameframework.GameInfo;
 
 import java.util.Random;
 
@@ -13,14 +17,21 @@ public class DumbAI extends GameComputerPlayer {
     }
 
     @Override
-    protected void receiveInfo(BattleShipGameState info) {
-
+    protected void receiveInfo(GameInfo info) {
+        BattleShipGameState gameState = new BattleShipGameState((BattleShipGameState)info);
+        Fire fire = new Fire(this);
+        PlaceShip placeShip = new PlaceShip(this);
         Random r = new Random();
         int row = r.nextInt(10) + 1;
         int col = r.nextInt(10) + 1;
 
-        info.printFire(row, col, true);
+        if (playerNum == gameState.getPlayersTurn()) {
+            Coordinates coord = new Coordinates(false, true, row, col);
+        }
+
+        gameState.printFire(row, col, true);
         Log.i("randomFire", "Fired at " + row + " " + col + "." );
+        game.sendAction(fire);
     //bruh
 
     }
